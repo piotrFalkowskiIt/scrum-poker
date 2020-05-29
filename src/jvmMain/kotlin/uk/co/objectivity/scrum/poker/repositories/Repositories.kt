a3c16bacb6aa630dbe8feb959100fc9374c8cbb9
Grzegorz Musiał <grzegorz@musial.tk>
2020-05-29 13:00:45 +0200

    chore: drop initial code that noone needs

--- src/jvmMain/kotlin/uk/co/objectivity/scrum/poker/repositories/Repositories.kt
+++
@@ -1,6 +0,0 @@
-package uk.co.objectivity.scrum.poker.repositories
-
-import org.springframework.data.repository.CrudRepository
-import uk.co.objectivity.scrum.poker.entities.Product
-
-interface ProductRepository : CrudRepository<Product, Long>
