64f9d6f8526c6c5b591a63727e0fed6cf0827ddf
Adam Kokot <akokot@objectivity.co.uk>
2020-05-29 11:19:59 +0200

    session # absolut paths (why???)

--- src/jvmMain/kotlin/uk/co/objectivity/scrum/poker/session/ScrumSessionController.kt
+++ src/jvmMain/kotlin/uk/co/objectivity/scrum/poker/session/ScrumSessionController.kt
@@ -3,20 +3,20 @@ package uk.co.objectivity.scrum.poker.session
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
-@RestController("/session")
+@RestController
 class ScrumSessionController (
         val sessionRepository: SessionRepository) {
 
-    @PostMapping
+    @PostMapping("/session")
     fun createSession() : ScrumSession {
         val sessionId = UUID.randomUUID()
         val session = ScrumSession(sessionId)
         return sessionRepository.save(session)
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/session/{id}")
     fun testGetSession(@PathVariable("id") sessionId: UUID) : ScrumSession {
-        return ScrumSession(UUID.randomUUID());
+        return ScrumSession(UUID.randomUUID())
         //return sessionRepository.findById(sessionId).orElseGet { "Session not found!" }
     }
 
