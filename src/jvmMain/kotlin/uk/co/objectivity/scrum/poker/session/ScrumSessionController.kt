626d169f85114c09cbcca08b3a6883e615e8f56c
Adam Kokot <akokot@objectivity.co.uk>
2020-05-29 15:56:40 +0200

    session # refactor

--- src/jvmMain/kotlin/uk/co/objectivity/scrum/poker/session/ScrumSessionController.kt
+++ src/jvmMain/kotlin/uk/co/objectivity/scrum/poker/session/ScrumSessionController.kt
@@ -6,13 +6,6 @@ import java.time.Instant
 import java.util.UUID
 import java.util.UUID.randomUUID
 
-internal class NewSessionRequest (val name: String)
-
-internal sealed class Response {
-    class SessionInfo (val id: UUID, val name: String, val created: Instant) : Response()
-    class SessionError (val message: String = "Unexpected error") : Response()
-}
-
 private val NOT_FOUND_RESPONSE = Response.SessionError("Session not found")
 
 @RequestMapping("/api/session")
@@ -21,19 +14,30 @@ private class ScrumSessionController (
         val sessionRepository: SessionRepository) {
 
     @PostMapping
-    private fun createSession(@RequestBody sessionData: NewSessionRequest) : Response {
-        return sessionRepository.save(newSession(sessionData.name)).mapToInfo()
-    }
+    private fun createSession(@RequestBody sessionData: NewSessionRequest) : Response =
+            sessionRepository.save(newSession(sessionData.name)).mapToInfo()
 
     @GetMapping("/{id}")
-    private fun getSessionInfo(@PathVariable("id") sessionId: UUID) : Response {
-        return sessionRepository.findById(sessionId)
-                .map{ it.mapToInfo() }
-                .orElseGet{ NOT_FOUND_RESPONSE }
-    }
+    private fun getSessionInfo(@PathVariable("id") sessionId: UUID) : Response =
+            sessionRepository.findById(sessionId)
+                    .map{ it.mapToInfo() }.orElseGet{ NOT_FOUND_RESPONSE }
 
 }
 
 private fun newSession(sessionName: String) : ScrumSession {
     return ScrumSession(randomUUID(), sessionName, Clock.systemUTC().instant())
+}
+
+internal class NewSessionRequest (val name: String)
+
+internal sealed class Response {
+
+    class SessionInfo (
+            val id: UUID,
+            val name: String,
+            val created: Instant): Response()
+
+    class SessionError (
+            val message: String = "Unexpected error"): Response()
+
 }
