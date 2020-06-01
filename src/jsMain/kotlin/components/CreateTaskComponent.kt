b6c99033fb82b752bdf3d7df23c6b60eb5036329
krabiega <kamilrabiega93@gmail.com>
2020-06-01 07:26:35 +0200

    tasks # simple input + button to allow user pass task name and create task itself then

--- src/jsMain/kotlin/components/CreateTaskComponent.kt
+++ src/jsMain/kotlin/components/CreateTaskComponent.kt
@@ -1,15 +1,45 @@
 package components
 
+import kotlinext.js.js
+import kotlinx.html.InputType
+import kotlinx.html.id
+import kotlinx.html.style
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
-import react.dom.p
+import react.dom.div
+import react.dom.input
 
 class CreateTaskComponent : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
-        p {
-            +"create task component added by extension function"
+        div {
+            attrs.style = js {
+                display = "flex"
+                flexDirection = "row"
+                border = "1px solid #9c0d03"
+                maxWidth = "400px"
+            }
+            input {
+                attrs.type = InputType.text
+                attrs.id = "taskTitle"
+                attrs.placeholder = "Task title..."
+                attrs.style = js {
+                    margin = "auto"
+                    marginTop = "10px"
+                    marginBottom = "10px"
+                }
+            }
+            input {
+                attrs.type = InputType.submit
+                attrs.value = "Create task"
+                attrs.style = js {
+                    margin = "auto"
+                    marginTop = "10px"
+                    marginBottom = "10px"
+                    minWidth = "100px"
+                }
+            }
         }
     }
 }
